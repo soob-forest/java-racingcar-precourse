1a08312acd5340ee22c7768144ccccfae0c31f83
soob-forest <dltntnqo@naver.com>
2022-04-25 00:55:24 +0900

    feat(Cars): 참가한 자동차 리스트를 관리하기 위한 도메인
    - 자동차를 참가시키기 위한 함수 추가
    - 참가한 자동차를 반환하기 위한 함수 추가(방어적 복사 기법 적용)
    - 우승한 자동차를 반환하기 위한 함수 추가(방어적 복사 기법 적용)
    - 중복된 이름 생성 validate 함수 추가

---
+++ src/main/java/racingcar/domain/cars/Cars.java
@@ -0,0 +1,52 @@
+package racingcar.domain.cars;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import racingcar.common.error.ErrorMessage;
+import racingcar.domain.car.Car;
+import racingcar.domain.car.Distance;
+
+public class Cars {
+    private List<Car> cars;
+
+    public Cars() {
+        cars = new ArrayList<>();
+    }
+
+    public void participate(Car newCar) {
+        validateDuplicatedName(newCar);
+        cars.add(newCar);
+    }
+
+    public List<Car> getParticipatedCars() {
+        return Collections.unmodifiableList(new ArrayList<>(cars));
+    }
+
+    public List<Car> getWinningCars() {
+
+        List<Distance> distances = new ArrayList<>();
+        for (Car car : cars) {
+            distances.add(car.getDistance());
+        }
+        distances.sort(Comparator.reverseOrder());
+
+        Distance maxDistance = distances.get(0);
+
+        List<Car> winningCars = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (car.getDistance().equals(maxDistance)) {
+                winningCars.add(car);
+            }
+        }
+        return Collections.unmodifiableList(new ArrayList<>(winningCars));
+    }
+
+    private void validateDuplicatedName(Car newCar) {
+        if (this.cars.contains(newCar)) {
+            throw new IllegalStateException(ErrorMessage.MSG_DUPLICATE_NAME.getMessage());
+        }
+    }
+}
