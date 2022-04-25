078adc1f96a593903d5847ba32855fcef826c6b6
soob-forest <dltntnqo@naver.com>
2022-04-25 21:35:32 +0900

    feat(RaceCount): race 반복 회수를 표현하는 Vo 추가

--- src/main/java/racingcar/common/error/ErrorMessage.java
+++ src/main/java/racingcar/common/error/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     MSG_INVALID_OVER_NAME_EMPTY("자동차의 이름은 공백일 수 없습니다."),
     MSG_INVALID_OVER_NAME_NULL("자동차의 이름은 NULL일 수 없습니다."),
     MSG_DUPLICATE_NAME("자동차의 이름은 중복 할 수 없습니다."),
-    MSG_INVALID_NUMBER("0과 9사이의 숫자만 가능합니다.");
+    MSG_INVALID_NUMBER("0과 9사이의 숫자만 가능합니다."),
+    MSG_INVALID_RACE_COUNT("양수만 가능합니다.");
 
     String message;
 
